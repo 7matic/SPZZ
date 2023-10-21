bad2fc1933fb94aaa1e70f9605ea57265fd50c03
7matic <7matic@protonmail.com>
2023-10-21 11:57:37 +0200

    Sample basic project, SvelteKit+Tailwind

---
+++ tpo/src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<footer
+    class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <p>
+            <b class="pr-2">GitHub</b>
+            <a href="https://github.com/7matic/tpo-group-project" target="_blank" class="text-violet-400"
+                >TPO project<sup class=""
+                    ><span class="text-xs scale-75 pl-0.5"
+                        ><i
+                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                        /></span
+                    ></sup
+                ></a
+            >
+        </p>
+    </div>
+</footer>
---
+++ tpo/src/components/Main.svelte
@@ -0,0 +1,211 @@
+<script>
+    import Step from "./Step.svelte";
+
+    let steps = [
+        {
+            name: "...",
+            icon: "fa-solid fa-cart-shopping",
+            description:
+                "?",
+        },
+        {
+            name: "...",
+            icon: "fa-solid fa-list-check",
+            description:
+                "?",
+        },
+        {
+            name: "...",
+            icon: "fa-solid fa-diagram-project",
+            description:
+                "?",
+        },
+    ];
+
+    let benefits = [
+        {
+            metric: "10x",
+            name: "-_-",
+            description:
+                "blablabla",
+        },
+        {
+            name: "-_-",
+            description:
+                "blablabla",
+        },
+        {
+            name: "-_-",
+            description:
+                "blablabla",
+        },
+    ];
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section
+        id="introPage"
+        class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
+    >
+        <div
+            class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+        >
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi! <span class="poppins text-violet-400">Igor Rožanc</span>
+                <br />is the best!
+            </h2>
+            <button
+                class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
+            >
+                <div
+                    class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+                />
+                <h4 class="relative z-9">Get in touch &rarr;</h4>
+            </button>
+        </div>
+        <div class="relative shadow-2xl grid place-items-center">
+            <img
+                src={"images/profile.png"}
+                alt="UL FRI"
+                class="object-cover z-[2] max-h-[70vh]"
+            />
+        </div>
+    </section>
+    <section class="py-20 lg:py-32 flex flex-col gap-24" id="projects">
+        <div class="flex flex-col gap-2 text-center">
+            <h6 class="text-large sm:text-xl md:text-2xl">
+                A few examples.
+            </h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                Curious to <span class="poppins text-violet-400">see</span> our work?
+            </h3>
+        </div>
+        <a
+            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            target="_blank"
+            class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
+        >
+            <i class="fa-regular fa-circle-play" />
+            <p>Watch the video</p>
+        </a>
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+            <Step step={steps[0]}>
+                <p>
+                    blablabla
+                </p>
+            </Step>
+            <Step step={steps[1]}>
+                <p>
+                    blablabla
+                </p>
+            </Step>
+            <Step step={steps[2]}>
+                <p>
+                blablabla
+                </p>
+            </Step>
+        </div>
+    </section>
+    <section
+        id="about"
+        class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+    >
+        <div
+            class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+        >
+            <h6 class="text-large sm:text-xl md:text-2xl">
+                Want to know more?
+            </h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                A bit <span class="poppins text-violet-400">about</span> us.
+            </h3>
+        </div>
+        <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">
+            We are . . .
+        </p>
+        <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+            {#each benefits as benefit, index}
+                <div class="flex gap-6 sm:gap-8">
+                    <p
+                        class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
+                    >
+                        0{index + 1}
+                    </p>
+                    <div class="flex flex-col gap-6 sm:gap-8">
+                        <h3 class="text-2xl sm:text-3xl md:text-5xl">
+                            {benefit.name}
+                        </h3>
+                        <p>{benefit.description}</p>
+                    </div>
+                </div>
+            {/each}
+        </div>
+        <h5 class={" text-2xl sm:text-3xl font-semibold text-center poppins "}>
+            The <span class="text-violet-400">Complete</span> Package
+        </h5>
+        <div
+            class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full"
+        >
+            <table class="bg-white text-slate-700 rounded text-center">
+                <thead class={"border-b border-solid border-slate-200  "}>
+                    <tr class="">
+                        <th />
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+                        <th
+                            class="bg-violet-700 text-white whitespace-nowrap p-4 px-8"
+                            >Us</th
+                        >
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td
+                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                            >Dedication</td
+                        >
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-green-500" /></td>
+                    </tr>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td
+                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                            >Critical Thought</td
+                        >
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-green-500" /></td>
+                    </tr>
+                    <tr>
+                        <td
+                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                            >Interpersonal Skills</td
+                        >
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-green-500" /></td>
+                    </tr>
+                    <tr class="border-t border-solid border-slate-200">
+                        <td
+                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                            >Programming Ability</td
+                        >
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-green-500" /></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="mx-auto -mt-12 italic sm:hidden opacity-50">
+            <p>Scroll to see more &rarr;</p>
+        </div>
+        <p class="mx-auto">Software engineering is the best!</p>
+    </section>
+</main>
