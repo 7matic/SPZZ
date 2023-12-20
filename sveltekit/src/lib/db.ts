d14edba938b8432ba0745cda52a49d047775bdca
Amadej <amadej.milicev99@gmail.com>
2023-12-20 16:55:20 +0100

    first draft schema, added seeding

---
+++ sveltekit/src/lib/db.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from '@prisma/client'
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+// use `prisma` in your application to read and write data in your DB
