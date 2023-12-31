05108e539e1a7d46bb2f833ce34698bee4c3fe78
Amadej <amadej.milicev99@gmail.com>
2023-12-31 15:06:26 +0100

    express server added

---
+++ express/src/index.ts
@@ -0,0 +1,39 @@
+import { Prisma, PrismaClient } from '@prisma/client'
+import express from 'express'
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.use(express.json())
+
+app.post(`/user`, async (req, res) => {
+  const { email, token } = req.body
+
+  let result = null;
+
+  if(email){
+  result = await prisma.user.findUnique({
+    where: {
+      email: String(email),
+    }
+  })
+  }
+  else if(token){
+    result = await prisma.user.findUnique({
+      where:{
+        auth0token: String(token)
+      }
+    })
+  }
+
+  if(result != null)
+    res.json(result)
+  else
+    res.json({error: "User not found!"})
+})
+
+const server = app.listen(3000, () =>
+  console.log(`
+🚀 Server ready at: http://localhost:3000
+⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`),
+)
