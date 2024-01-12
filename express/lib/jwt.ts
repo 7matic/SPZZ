5ef1ef81662dcbdd1eee6d757fd5074facb20fc8
Amadej <amadej.milicev99@gmail.com>
2024-01-12 10:45:54 +0100

    fixed jwt verification

--- express/lib/jwt.ts
+++ express/lib/jwt.ts
@@ -1,12 +1,8 @@
-import { Request, Response, NextFunction } from 'express';
+import { Response, NextFunction } from 'express';
 import { sign, decode, verify } from 'jsonwebtoken';
-
-export interface IGetUserAuthInfoRequest extends Request {
-    user: string // or any other type
-}
+import { IGetUserAuthInfoRequest } from './types';
 
 export function generateAccessToken(user_id: number, role: string, company_id?: number) {
-    console.log(process.env.JWT_SECRET);
     return sign({
         id: user_id,
         role: role,
@@ -15,18 +11,13 @@ export function generateAccessToken(user_id: number, role: string, company_id?: 
 }
 
 export function verifyAccessToken(req: IGetUserAuthInfoRequest, res: Response, next: NextFunction) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]
+    const token = req.headers['authorization'];
 
-    if (token == null) return res.status(400);
+    if (token == undefined) return res.status(400);
 
-    verify(token, process.env.TOKEN_SECRET as string, (err: any, user: any) => {
-        console.log(err)
-
+    verify(token, process.env.JWT_SECRET as string, (err: any, user: any) => {
         if (err) return res.status(403);
-
         req.user = user;
-
         next();
     })
 }
