640adf692c07702236a0b7cd96b7254d8245a39a
Amadej <amadej.milicev99@gmail.com>
2024-01-12 13:49:17 +0100

    errors handled

--- express/lib/jwt.ts
+++ express/lib/jwt.ts
@@ -10,16 +10,24 @@ export function generateAccessToken(user_id: number, role: string, company_id?: 
     }, process.env.JWT_SECRET as string, { expiresIn: '15m' });
 }
 
+// TODO: Non blocking middleware
+
 export function verifyAccessToken(req: IGetUserAuthInfoRequest, res: Response, next: NextFunction) {
     const token = req.headers['authorization'];
 
-    if (token == undefined) return res.status(400);
+    if (!token) {
+        console.log('Token is missing or undefined');
+        return res.status(400).json({ error: 'Token missing or undefined' });
+    }
 
     verify(token, process.env.JWT_SECRET as string, (err: any, user: any) => {
-        if (err) return res.status(403);
+        if (err) {
+            console.error(err);
+            return res.status(403).json({ error: 'Token verification failed' });
+        }
         req.user = user;
         next();
-    })
+    });
 }
 
 export function generateRefreshToken(user_id: string) {
