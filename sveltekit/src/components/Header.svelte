e29f4fc3fd51c1c93d626db61834da2b5c6c15d5
krisp3t <77499501+krisp3t@users.noreply.github.com>
2024-01-13 02:31:38 +0100

    Improve Header

--- sveltekit/src/components/Header.svelte
+++ sveltekit/src/components/Header.svelte
@@ -5,30 +5,25 @@
     export let y;
 
     export let tabs = [
-        {name: "Projects ", link: "#projects"},
-        {name: "About us", link: "#about"},
-        {name: "Blog", link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"},
+        {name: "Prijavi se", link: "#projects"},
     ];
 </script>
 
 <header
-        class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid " +
-        (y > 0
-            ? " py-4 bg-slate-950 border-violet-950"
-            : " py-6 bg-transparent border-transparent")}
+        class={"sticky z-[10] top-0 duration-200 px-6 flex flex-col items-center justify-between border-b border-solid py-4 bg-dark border-gray-800 shadow-outline-primary"}
 >
     <Logo size="64"/>
-    <div class="sm:flex items-center gap-4 hidden">
+    <div class="sm:flex items-center gap-4 hidden font-sans">
         {#each tabs as tab, index}
             <a
                     href={tab.link}
-                    class="duration-200 hover:text-violet-400"
+                    class="duration-200 hover:text-primary"
                     target={index === 2 ? "_blank" : ""}
             >
                 <p>{tab.name}</p>
             </a>
         {/each}
-        <Button text="Objavi oglas"
+        <Button text="Preglej oglase"
                 variation="primary"
         />
     </div>
