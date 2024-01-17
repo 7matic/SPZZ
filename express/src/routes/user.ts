2ac73ccf539d77aa5bae240c779aecc97406a048
Amadej <amadej.milicev99@gmail.com>
2024-01-17 17:56:44 +0100

    /user/applications added

--- express/src/routes/user.ts
+++ express/src/routes/user.ts
@@ -69,28 +69,51 @@ userRouter.get(`/`, async (req, res) => {
 userRouter.put(`/update`, verifyAccessToken, async (req: IGetUserAuthInfoRequest, res) => {
     const { body } = req;
 
-    const result = await prisma.user.update({
-        where: {
-            id: req.user?.id,
-        },
-        data: {
-            firstName: body.firstName,
-            lastName: body.lastName,
-            email: body.email,
-            phoneNumber: body.phoneNumber,
-            location: body.location,
-            designations: body.designations,
-            colleges: body.colleges,
-            degrees: body.degrees,
-            workExperience: body.workExperience,
-            skills: body.skills,
-        }
-    });
+    try {
+        const result = await prisma.user.update({
+            where: {
+                id: req.user?.id,
+            },
+            data: {
+                firstName: body.firstName,
+                lastName: body.lastName,
+                email: body.email,
+                phoneNumber: body.phoneNumber,
+                location: body.location,
+                designations: body.designations,
+                colleges: body.colleges,
+                degrees: body.degrees,
+                workExperience: body.workExperience,
+                skills: body.skills,
+            }
+        });
 
-    if (result != null)
-        res.json(result)
-    else
-        res.json({ error: "Update failed!" });
+        return res.json(result);
+    }
+
+    catch (e) {
+        return res.json({ error: "Update failed!" });
+    }
+});
+
+userRouter.get('/applications', verifyAccessToken, async (req: IGetUserAuthInfoRequest, res) => {
+    try {
+        const result = await prisma.jobOffer.findMany({
+            where: {
+                applicants: {
+                    some: {
+                        id: req.user?.id,
+                    }
+                }
+            }
+        });
+
+        return res.json(result);
+    }
+    catch (e) {
+        console.log(e);
+        return res.json({ error: "Error fetching applications!" });
+    }
 });
 
 userRouter.get('/withToken', verifyAccessToken as RequestHandler, async (req: IGetUserAuthInfoRequest, res) => {
