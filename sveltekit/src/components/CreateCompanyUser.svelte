c0a1962633cbf51fcf49d37468687358514b3526
sp9028 <sp9028@student.uni-lj.si>
2024-01-17 14:06:46 +0100

    Added logout to company create

--- sveltekit/src/components/CreateCompanyUser.svelte
+++ sveltekit/src/components/CreateCompanyUser.svelte
@@ -2,11 +2,13 @@
     import Button from "./Button.svelte";
     import {getUser, makeRequest} from "../api/api";
 
+
     let name: string = '';
     let logo: string = '';
     let location: string = '';
     let errorMessage: string | null = null;
     let isLoading: boolean = false;
+    let showPopup: boolean = false;
 
     async function handleSubmit() {
         isLoading = true;
@@ -24,12 +26,17 @@
             });
             const user = await getUser();
             localStorage.setItem('user', JSON.stringify(user));
-            window.location.href = '/profile';
+			showPopup = true;
         } catch (error) {
             errorMessage = error.message;
         } finally {
             isLoading = false;
+			
         }
+    }
+
+    function onClose(){
+        window.location.href = '/login';
     }
 </script>
 
@@ -51,4 +58,15 @@
     {#if errorMessage}
         <p class="mt-4 text-red-500">{errorMessage}</p>
     {/if}
+    {#if showPopup}
+    <div class="fixed inset-0 flex items-center justify-center">
+      <div class="bg-black bg-opacity-50 w-full h-full absolute"></div>
+      <div class="bg-white p-8 rounded shadow-md z-10 relative max-w-lg text-center">
+        <p class="text-gray-700">Za shranitev podatkov je potrebna ponovna prijava.</p>
+        <Button on:click={onClose}>
+          Ponovna prijava
+        </Button>
+      </div>
+    </div>
+  {/if}
 </form>
